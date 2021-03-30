fd43bd56b51e408e22e40519bed2d5a66713b34c
xoreo <mattnappo@gmail.com>
2021-03-30 01:04:17 -0400

    Cleaned up interface.

--- src/perlin.rs
+++ src/perlin.rs
@@ -1,14 +1,11 @@
-use ggez;
-use ggez::event;
-use ggez::graphics::{self, Color};
-use ggez::{Context, GameResult};
+use ggez::{
+    self, event,
+    graphics::{self, Color},
+    {Context, GameResult},
+};
 use glam::*;
-use rand;
-use rand::rngs::StdRng;
-use rand::Rng;
-use rand::SeedableRng;
-use std::f32::consts;
-use std::time::SystemTime;
+use rand::{self, rngs::StdRng, Rng, SeedableRng};
+use std::{f32::consts, time::SystemTime};
 
 const PI2: f32 = 2.0 * consts::PI;
 
@@ -31,33 +28,49 @@ const RED: Color = Color {
     a: 1.0,
 };
 
-// Helper method to interpolate between two values with the given weight.
+// Helper method to interpolate between two values.
 fn interpolate(a: f32, b: f32, w: f32) -> f32 {
+    // check that w \in [0, 1]
     (a - b) * (3.0 - w * 2.0) * w * w + a
 }
 
+/// The game's main state
 struct State {
+    /// Number of columns (of vectors in x dir)
     x: usize,
+
+    /// Number of rows (of vectors in y dir)
     y: usize,
-    vecs: Vec<Vec<Vec2>>, // The random vecs
-    map: Vec<Vec<f32>>,   // The final interpolated map
+
+    /// The initial random unit gradient vectors (unit = 1.0)
+    vecs: Vec<Vec<Vec2>>,
+
+    /// The final, interpolated noise. Each f32 is a value in the range [0, 1],
+    /// representing the color value of a pixel at the point (x, y).
+    /// There are x*scale x values, and y*scale y values.
+    noise: Vec<Vec<f32>>,
+
+    /// All game randomness comes from this rng
     prng: StdRng,
+
+    // The rng's seed
     seed: u64,
-    box_size: f32,
+
+    /* -- For rendering only -- */
+    /// Whether the scene was drawn already
     drawn: bool,
-    scale: f32,
+
+    /// Linear shift, in pixels (for rendering only)
     shift: f32,
+
+    /// The number of pixels between each grid, the length of the arrows
+    /// when drawn (in pixels)
+    scale: f32,
 }
 
 impl State {
-    fn new(
-        x: usize,
-        y: usize,
-        box_size: f32,
-        scale: f32,
-        shift: f32,
-        seed: Option<u64>,
-    ) -> GameResult<Self> {
+    /// Create a new game state
+    fn new(x: usize, y: usize, scale: f32, shift: f32, seed: Option<u64>) -> GameResult<Self> {
         // Build the prng
         let mut use_seed: u64 = 0;
         if let Some(s) = seed {
@@ -71,13 +84,12 @@ impl State {
         let mut prng = StdRng::seed_from_u64(use_seed);
 
         let mut state = Self {
-            x: x - 1,
-            y: y - 1,
-            map: Vec::new(),
+            x: x,
+            y: y,
             vecs: Vec::new(),
+            noise: Vec::new(),
             prng,
             seed: use_seed,
-            box_size,
             drawn: false,
             scale,
             shift,
@@ -87,76 +99,42 @@ impl State {
         Ok(state)
     }
 
+    /// Generate a random vector of magnitude 1.0 with this state's prng
     fn random_unit_vector(&mut self) -> Vec2 {
         let a = self.prng.gen_range(0.0, PI2);
-        Vec2::new(self.scale * a.cos(), self.scale * a.sin())
+        Vec2::new(a.cos(), a.sin())
     }
 
+    /// Generate the random vectors
     fn gen_vecs(&mut self) {
         // Step 1
         // Assign a random vector to each point in the grid
-        for x in 0..=self.x {
+        for x in 0..self.x {
             let mut row: Vec<Vec2> = Vec::new();
-            for y in 0..=self.y {
+            for y in 0..self.y {
                 row.push(self.random_unit_vector());
             }
             self.vecs.push(row);
         }
     }
 
+    /// Calculate the dot products and interpolation
     fn calc_perlin(&mut self) {
         // Step 2 and 3. Calc dot products and interpolate.
-        for x in 0..((self.box_size * (self.x - 0) as f32) as usize) {
-            let mut row: Vec<f32> = Vec::new();
-            for y in 0..((self.box_size * (self.y - 0) as f32) as usize) {
-                //let gridx = (x as f32 % (self.box_size / self.x as f32)) as usize;
-                //let gridy = (y as f32 % (self.box_size / self.y as f32)) as usize;
-                let gridx = (x / self.box_size as usize) % self.x;
-                let gridy = (y / self.box_size as usize) % self.y;
-                let gridxf = gridx as f32;
-                let gridyf = gridy as f32;
-
-                let off_ul = Vec2::new((x - gridx) as f32, (y - gridy) as f32);
-                let off_ur = Vec2::new(x - (gridx as f32 + self.box_size), y - gridy);
-                let off_bl = Vec2::new(x - gridx, y - (gridy + self.box_size));
-                let off_br = Vec2::new(x - gridx, y - gridy);
-
-                println!("real: (x,y) = ({}, {})", x, y);
-                println!("grid: (x,y) = ({}, {})", gridx, gridy);
-                println!(" off: (x,y) = ({}, {})\n", x - gridx, y - gridy);
-                //println!("x, y = {}, {} --> ({}, {})", x, y, gridx, gridy);
-                // let gridy: usize = y % (self.y as usize);
-                //println!("{} {}", gridx, gridy);
-
-                let dot1 =
-                    Vec2::new((x - gridx) as f32, (y - gridy) as f32).dot(self.vecs[gridy][gridx]);
-                let dot2 = Vec2::new((x - gridx + 1) as f32, (y - gridy) as f32)
-                    .dot(self.vecs[gridy][gridx + 1]);
-                let dot3 = Vec2::new((x - gridx) as f32, (y - gridy + 1) as f32)
-                    .dot(self.vecs[gridy + 1][gridx]);
-                let dot4 = Vec2::new((x - gridx + 1) as f32, (y - gridy + 1) as f32)
-                    .dot(self.vecs[gridy + 1][gridx + 1]);
-
-                // Temp
-                let i1 = interpolate(dot1, dot2, (x - gridx) as f32);
-                let i2 = interpolate(dot3, dot4, (x - gridx) as f32);
-                let i = interpolate(i1, i2, (y - gridy) as f32);
-
-                row.push(i);
-            }
-            self.map.push(row);
-        }
     }
 
+    /* -- Drawing functions -- */
+
+    /// Draw the lattice grid
     fn draw_grid(&self, ctx: &mut Context) -> GameResult {
-        let bs = self.box_size as f32;
-        for x in 0..=self.x {
+        let bs = self.scale as f32;
+        for x in 0..self.x {
             let xf = x as f32;
             let line = graphics::Mesh::new_line(
                 ctx,
                 &[
                     Vec2::new(xf * bs + self.shift, self.shift),
-                    Vec2::new(xf * bs + self.shift, bs * self.y as f32 + self.shift),
+                    Vec2::new(xf * bs + self.shift, bs * (self.y - 1) as f32 + self.shift),
                 ],
                 1.0,
                 RED,
@@ -164,13 +142,13 @@ impl State {
             graphics::draw(ctx, &line, (Vec2::new(0.0, 0.0),))?;
         }
 
-        for y in 0..=self.y {
+        for y in 0..self.y {
             let yf = y as f32;
             let line = graphics::Mesh::new_line(
                 ctx,
                 &[
                     Vec2::new(self.shift, yf * bs + self.shift),
-                    Vec2::new(self.shift + bs * self.x as f32, yf * bs + self.shift),
+                    Vec2::new(self.shift + bs * (self.x - 1) as f32, yf * bs + self.shift),
                 ],
                 1.0,
                 RED,
@@ -180,10 +158,14 @@ impl State {
         Ok(())
     }
 
+    /// Draw a 2D arrow/vector
     fn draw_vector(&self, ctx: &mut Context, v: Vec2, pos: (f32, f32)) -> GameResult {
         let line = graphics::Mesh::new_line(
             ctx,
-            &[Vec2::new(pos.0, pos.1), Vec2::new(pos.0 + v.x, pos.1 + v.y)],
+            &[
+                Vec2::new(pos.0, pos.1),
+                Vec2::new(pos.0 + v.x * self.scale, pos.1 + v.y * self.scale),
+            ],
             1.0,
             BLACK,
         )?;
@@ -193,10 +175,12 @@ impl State {
             ctx,
             &[
                 Vec2::new(
-                    (pos.0 + v.x) - head_length * (vec_angle + consts::FRAC_PI_4).sin(),
-                    (pos.1 + v.y) + head_length * (vec_angle + consts::FRAC_PI_4).cos(),
+                    (pos.0 + v.x * self.scale)
+                        - head_length * (vec_angle + consts::FRAC_PI_4).sin(),
+                    (pos.1 + v.y * self.scale)
+                        + head_length * (vec_angle + consts::FRAC_PI_4).cos(),
                 ),
-                Vec2::new(pos.0 + v.x, pos.1 + v.y),
+                Vec2::new(pos.0 + v.x * self.scale, pos.1 + v.y * self.scale),
             ],
             1.0,
             BLACK,
@@ -205,10 +189,12 @@ impl State {
             ctx,
             &[
                 Vec2::new(
-                    (pos.0 + v.x) + head_length * (vec_angle - consts::FRAC_PI_4).sin(),
-                    (pos.1 + v.y) - head_length * (vec_angle - consts::FRAC_PI_4).cos(),
+                    (pos.0 + v.x * self.scale)
+                        + head_length * (vec_angle - consts::FRAC_PI_4).sin(),
+                    (pos.1 + v.y * self.scale)
+                        - head_length * (vec_angle - consts::FRAC_PI_4).cos(),
                 ),
-                Vec2::new(pos.0 + v.x, pos.1 + v.y),
+                Vec2::new(pos.0 + v.x * self.scale, pos.1 + v.y * self.scale),
             ],
             1.0,
             BLACK,
@@ -220,15 +206,16 @@ impl State {
         Ok(())
     }
 
+    /// Draw all the random vectors on the grid
     fn draw_vectors(&self, ctx: &mut Context) -> GameResult {
-        for x in 0..=self.x {
-            for y in 0..=self.y {
+        for x in 0..self.x {
+            for y in 0..self.y {
                 self.draw_vector(
                     ctx,
                     self.vecs[y][x],
                     (
-                        x as f32 * self.box_size + self.shift,
-                        y as f32 * self.box_size + self.shift,
+                        x as f32 * self.scale + self.shift,
+                        y as f32 * self.scale + self.shift,
                     ),
                 )?;
             }
@@ -236,20 +223,8 @@ impl State {
         Ok(())
     }
 
-    fn draw_map(&self, ctx: &mut Context) -> GameResult {
-        for x in 0..(self.box_size * self.x as f32) as usize {
-            for y in 0..(self.box_size * self.y as f32) as usize {
-                let b = self.map[y][x] / (self.x as f32 * self.box_size * self.scale);
-                //println!("{}", b);
-                let px = graphics::Mesh::new_rectangle(
-                    ctx,
-                    graphics::DrawMode::fill(),
-                    graphics::Rect::new(x as f32 + self.shift, y as f32 + self.shift, 1.0, 1.0),
-                    Color::new(0.0, 0.0, b, 1.0),
-                )?;
-                graphics::draw(ctx, &px, (Vec2::new(0.0, 0.0),))?;
-            }
-        }
+    /// Draw the noise
+    fn draw_noise(&self, ctx: &mut Context) -> GameResult {
         Ok(())
     }
 }
@@ -268,7 +243,7 @@ impl event::EventHandler for State {
 
         self.draw_grid(ctx)?;
         self.draw_vectors(ctx)?;
-        self.draw_map(ctx)?;
+        self.draw_noise(ctx)?;
 
         self.drawn = true;
 
@@ -302,14 +277,13 @@ fn main() -> GameResult {
         .window_setup(w_setup)
         .window_mode(w_mode);
     let (mut ctx, mut event_loop) = cb.build()?;
+
     let mut state = State::new(
-        3,    // Number of cols
-        3,    // Number of rows
-        10.0, // Box size
-        10.0, // Scale
-        //40.0,  // Vector scale
-        5.0,  // Rendering shift
-        None, // Seed
+        3,     // Number of cols
+        3,     // Number of rows
+        100.0, // Scale
+        100.0, // Rendering shift
+        None,  // Seed
     )?;
     event::run(&mut ctx, &mut event_loop, &mut state)
 }
